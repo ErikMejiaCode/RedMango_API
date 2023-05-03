1fb8ae6c30c4cca257256b62c897755456e3ec96
Erik Mejia <erikdojo352@gmail.com>
2023-05-03 13:18:10 -0700

    Post Create Order route

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,138 @@
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using RedMango_API.Models.Dto;
+using RedMango_API.Utility;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace RedMango_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        //Getting orders 
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetOrders(string? userId)
+        {
+            try
+            {
+                var orderHeaders = _db.OrderHeaders
+                .Include(u => u.OrderDetails)
+                .ThenInclude(u => u.MenuItem)
+                .OrderByDescending(u => u.OrderHeaderId);
+
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    _response.Result = orderHeaders.Where(u => u.ApplicationUserId == userId);
+                }
+                else 
+                {
+                    _response.Result = orderHeaders;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        //Getting orders based on userId
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> GetOrders(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var orderHeaders = _db.OrderHeaders
+                .Include(u => u.OrderDetails)
+                .ThenInclude(u => u.MenuItem)
+                .Where(u => u.OrderHeaderId == id);
+
+                if (orderHeaders == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+                _response.Result = orderHeaders;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateOrder([FromBody] OrderHeaderCreateDTO orderHeaderDTO)
+        {
+            try 
+            {
+                OrderHeader order = new()
+                {
+                  ApplicationUserId = orderHeaderDTO.ApplicationUserId,  
+                  PickupEmail = orderHeaderDTO.PickupEmail,  
+                  PickupName = orderHeaderDTO.PickupName,  
+                  PickupPhoneNumber = orderHeaderDTO.ApplicationUserId,  
+                  OrderTotal = orderHeaderDTO.OrderTotal,  
+                  OrderDate = DateTime.Now,  
+                  StripedPaymentIntentId = orderHeaderDTO.StripedPaymentIntentId,  
+                  TotalItems = orderHeaderDTO.TotalItems,  
+                  Status = String.IsNullOrEmpty(orderHeaderDTO.Status)? SD.status_pending : orderHeaderDTO.Status
+                };
+
+                if (ModelState.IsValid)
+                {
+                    _db.OrderHeaders.Add(order);
+                    _db.SaveChanges();
+
+                    foreach(var OrderDetailsDTO in orderHeaderDTO.OrderDetailsDTO)
+                    {
+                        OrderDetails orderDetails = new()
+                        {
+                            OrderHeaderId = order.OrderHeaderId,
+                            ItemName = OrderDetailsDTO.ItemName,
+                            MenuItemId = OrderDetailsDTO.MenuItemId,
+                            Price = OrderDetailsDTO.Price,
+                            Quantity = OrderDetailsDTO.Quantity
+                        };
+                        _db.OrderDetails.Add(orderDetails);
+                    }
+                    _db.SaveChanges();
+                    _response.Result = order;
+                    order.OrderDetails = null;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+    }
+}
